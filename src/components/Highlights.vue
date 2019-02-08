2736e224c5039564a9e5358037757d1757a575a5
Gonzalo Garcia <gonzalo.garcia@interactive-tools.de>
2019-02-08 12:45:17 +0100

    first commit

---
+++ src/components/Highlights.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="custom-content-card content-card card">
+        <div class="card-body pb-0">
+            <div class="content-header h4 text-center pt-2 pb-3">Destacados</div>
+            <div class="row">
+                <uv-index :highlights="highlights"></uv-index>
+                <visibility :highlights="highlights"></visibility>
+                <wind-status :highlights="highlights"></wind-status>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UVIndex from './UVIndex';
+    import Visibility from './Visibility';
+    import WindStatus from './WindStatus';
+
+	export default {
+		props: ["highlights"],
+		components: {
+			'uv-index': UVIndex,
+			'visibility': Visibility,
+			'wind-status': WindStatus,
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+
+		},
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="search">
+        <input
+                id="location-input"
+                type="text"
+                ref="input"
+                placeholder="Lugar?"
+                @keyup.enter="organizeAllDetails"
+        >
+        <button id="search-btn" @click="organizeAllDetails">
+            <img src="./assets/Search.svg" width="24" height="24">
+        </button>
+        <label> {{ location }}</label>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "Search"
+	}
+</script>
+
+<style scoped>
+
+</style>
